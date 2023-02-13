f72b6e3020dba5b38996058d0fe47f8690c6f19c
vjbytes102 <vy386@nyu.edu>
2023-02-14 02:51:47 +0530

    shm_size

--- run_docker.py
+++ run_docker.py
@@ -175,7 +175,7 @@ def main(syn, args):
                                               detach=True, volumes=volumes,
                                               name=args.submissionid,
                                               network_disabled=True,
-                                              mem_limit='20g', stderr=True)
+                                              mem_limit='10g',shm_size='10g', stderr=True)
         except docker.errors.APIError as err:
             remove_docker_container(args.submissionid)
             errors = str(err) + "\n"
